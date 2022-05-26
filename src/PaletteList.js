e0fd20e75b4f0771ea97d0aba9699e8988a26ec7
Colin Breeding <93558819+colinmb5@users.noreply.github.com>
2022-05-26 16:17:16 -0400

    Start Styling PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import './PaletteList.css';
 import MiniPalette from "./MiniPalette";
 
 class PaletteList extends Component {
     render() {
         const { palettes } = this.props;
         return (
-            <div>
-                <h1>React Colors</h1>
-                {palettes.map(palette =>
-                    <MiniPalette {...palette} />
-                )}
+            <div className='PaletteList-root'>
+                <div className='PaletteList-container'>
+                    <nav className='PaletteList-nav'>
+                        <h1>React Colors</h1>
+                    </nav>
+                    <div className='PaletteList-palettes'>
+                        {palettes.map(palette =>
+                            <MiniPalette {...palette} />
+                        )}
+                    </div>
+                </div>
             </div>
         );
     }
